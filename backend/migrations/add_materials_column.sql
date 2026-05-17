ae78fe3917b81b39c0cd7bdf652c578409ffc29d
nhanggwp <trungnhanspotify@gmail.com>
2026-05-17 10:29:14 +0700

    chore: gitignore backend/migrations and backend/sql

--- backend/migrations/add_materials_column.sql
+++
@@ -1,5 +0,0 @@
--- Add materials JSONB column to session table for storing simple material names
-ALTER TABLE tutor_system.session 
-ADD COLUMN IF NOT EXISTS materials JSONB DEFAULT '[]'::jsonb;
-
-COMMENT ON COLUMN tutor_system.session.materials IS 'List of material file names (JSONB array)';
